be6b53aba9ee34053aa9b559acd3868f7dbe114c
jhpx <jhpx@hotmail.com>
2015-11-22 16:34:28 +0800

    Fix a menu bug

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -57,7 +57,7 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.detailfragment, menu);
+        inflater.inflate(R.menu.forecastfragment, menu);
 
     }
 
